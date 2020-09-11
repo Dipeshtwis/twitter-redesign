a1338610cd2e496bf0c661fb2b1e60b038c093dc
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-09-12 03:47:48 +0530

    Add registration form and navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'users/show'
+  resources :users, only: %i[new create edit update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
