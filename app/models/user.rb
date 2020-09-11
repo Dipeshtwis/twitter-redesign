e531779b1f2cf9455218d1a1f998cd4fec6710a5
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-09-11 19:35:26 +0530

    Add user model validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
   has_one_attached :photo
   has_one_attached :cover_image
+
+  validates :username, presence: true, uniqueness: true, length: { minimum: 3 }
+  validates :fullname, presence: true, length: {minimum: 5, maximum: 30}
+  validates_attachment_content_type :photo, :cover_image,  
+                      :content_type => ["image/jpg", "image/jpeg", "image/png"]
 end
