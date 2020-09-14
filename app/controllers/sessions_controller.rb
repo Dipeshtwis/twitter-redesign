7aefe26c54b0e8b0d1d51324a73d987ca8b0fd22
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-09-14 14:22:50 +0530

    Add login form, routes, and color of navbar

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,9 +7,10 @@ class SessionsController < ApplicationController
 
     if @user
       session[:username] = @user.username
+      flash[:success] = 'successfully signed in'
       redirect_to root_path
     else
-      flash.now[:danger] = 'Something wrong with the login information'
+      flash.now[:danger] = 'Something went wrong...'
       render 'new'
     end
   end
@@ -17,6 +18,6 @@ class SessionsController < ApplicationController
   def destroy
   	session[:username] = nil
     flash[:success] = 'You have successfully logged out'
-    redirect_to root_path
+    redirect_to sign_in_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get 'sessions/new'
   resources :users, only: %i[new create edit update]
+  get 'sign_in', to: 'sessions#new'
+  post 'sign_in', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
