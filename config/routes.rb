ddd68b167f96566f7e4266f0cbb3500092db8af6
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-09-15 15:27:00 +0530

    Configure route

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   post 'sign_in', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   post 'opinion', to: 'opinions#create'
+  post 'follow', to: 'followings#create'
+  get 'users/:username', to: 'users#show', as: 'profile'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
