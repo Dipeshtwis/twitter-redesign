9381b5aee9d11f520134788b00893bd142e2b798
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-09-16 01:37:33 +0530

    Add profile-pic, cover-pic, and render partial after-login

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+  def after_login(partial)
+  	render partial: partial if logged_in?
+  end
+
+  def profile_pic(user)
+  	if user.photo.present?
+      image_tag(user.photo.to_s, alt: user.username, class: "pr-2")
+    else
+      image_tag('https://source.unsplash.com/random/60x60',
+                alt: user.username, class: 'pr-2')
+    end
+  end
+
+  def cover_pic(user)
+  	if user.cover_image.present?
+      image_tag(user.cover_image.to_s, alt: user.username, class: 'pr-2')
+    else
+      image_tag('https://source.unsplash.com/random/700x250', alt: user.username, class: 'pr-2')
+    end
+  end
 end
