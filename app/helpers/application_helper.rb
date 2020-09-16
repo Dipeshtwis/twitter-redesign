85a4caa1bb854547cf7ba5f6ac5895a2cd1e223a
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-09-17 01:49:07 +0530

    Add user show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,10 +5,10 @@ module ApplicationHelper
 
   def profile_pic(usr, size = '')
   	if usr.photo.present?
-      image_tag(usr.photo.to_s, alt: usr.username, class: "pr-2 profile-img #{size}")
+      image_tag(usr.photo.to_s, alt: usr.username, class: " rounded-circle profile-img #{size}")
     else
       image_tag('https://source.unsplash.com/random/60x60',
-                alt: usr.username, class: 'pr-2')
+                alt: usr.username, class: 'pr-2 rounded-circle')
     end
   end
 
@@ -16,7 +16,8 @@ module ApplicationHelper
   	if usr.cover_image.present?
       image_tag(usr.cover_image.to_s, alt: usr.username, class: 'pr-2')
     else
-      image_tag('https://source.unsplash.com/random/700x250', alt: usr.username, class: 'pr-2')
+      image_tag('https://source.unsplash.com/random/700x250',
+      	        alt: usr.username, class: 'pr-2')
     end
   end
 
