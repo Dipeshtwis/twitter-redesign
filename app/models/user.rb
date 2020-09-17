77b56755c6fa2cc2b921cf1003f30c6904b8623d
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-09-17 16:39:25 +0530

    Add association between comment, opinion, and user

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
                                     content_type: ['image/jpg', 'image/jpeg', 'image/png']
 
   has_many :opinions, foreign_key: 'author_id', dependent: :destroy
+  has_many :comments, dependent: :destroy
   has_many :followings, class_name: 'Following', foreign_key: 'follower_id'
   has_many :follows, through: :followings, source: :followed
   has_many :inverse_followings, class_name: 'Following', foreign_key: 'followed_id'
