119547e1cf6d0fc32e7f2279e55dff671e3a3344
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-09-17 16:22:57 +0530

    Update the style of the views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,7 +8,7 @@ module ApplicationHelper
       image_tag(usr.photo.to_s, alt: usr.username, class: "pr-2 rounded-circle profile-img #{size}")
     else
       image_tag('https://source.unsplash.com/random/60x60',
-                alt: usr.username, class: 'pr-2 rounded-circle')
+                alt: usr.username, class: 'pr-2 rounded-circle profile-img')
     end
   end
 
