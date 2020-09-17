fcf63ad226a00693e05a049484b0b916c0063a0c
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-09-18 02:04:46 +0530

    Fix rubocop errors

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature "Users", type: :feature do
+RSpec.feature 'Users', type: :feature do
   before :each do
     @usr1 = User.create(username: 'deep', fullname: 'Deepesh Kumar')
     @usr2 = User.create(username: 'twister', fullname: 'Wingsform')
@@ -10,24 +10,24 @@ RSpec.feature "Users", type: :feature do
   end
 
   it 'visit own profile page' do
-  	visit profile_path(@usr1.username)
-  	expect(page).to have_text('ALL BOOKS SUGGESTED BY DEEPESH KUMAR')
+    visit profile_path(@usr1.username)
+    expect(page).to have_text('ALL BOOKS SUGGESTED BY DEEPESH KUMAR')
   end
 
   it 'visit other profile page' do
-  	visit 'users/twister'
-  	expect(page).to have_text('ALL BOOKS SUGGESTED BY WINGSFORM')
+    visit 'users/twister'
+    expect(page).to have_text('ALL BOOKS SUGGESTED BY WINGSFORM')
   end
 
   it 'follow a user which has profile page open' do
-  	visit 'users/twister'
-  	expect(page).to have_text('0 Following')
+    visit 'users/twister'
+    expect(page).to have_text('0 Following')
     find('a.follow_me').click
     expect(page).to have_text('1 Following')
   end
 
   it 'should logout' do
-  	click_link 'Sign out'
-  	expect(current_path).to eql(sign_in_path)
+    click_link 'Sign out'
+    expect(current_path).to eql(sign_in_path)
   end
 end
