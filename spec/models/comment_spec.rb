8a34bb4cebbf12bed669baff722ae3ca55dcbaba
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-09-17 18:08:26 +0530

    Add rails test environment and test comment model

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validation comment test' do
+    it { should validate_presence_of(:content) }
+
+    it do
+      should validate_length_of(:content)
+        .is_at_most(200)
+        .with_message('maximum 200 characters allowed.')
+    end
+  end
 end
