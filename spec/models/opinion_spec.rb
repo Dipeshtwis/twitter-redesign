30d0e91c3fa497dfaedaa51229f6e3fa3ec1da81
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-09-17 21:01:07 +0530

    Add user request test

--- spec/models/opinion_spec.rb
+++ spec/models/opinion_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Opinion, type: :model do
   describe 'opinion Association' do
-  	it { should belong_to(:author) }
+    it { should belong_to(:author) }
   end
 end
