a06bef4a752f5ec5afe82e49c5222c83a52546ff
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-09-18 02:35:15 +0530

    Updates for heroku deployment

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,10 +8,6 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("bootstrap")
-import toastr from 'toastr/toastr';
-
-// stylesheets
-require("../stylesheets/main.scss")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
